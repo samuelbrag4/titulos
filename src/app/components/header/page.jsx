a601056bcf92343eaa194ceea2b35bfb5b1e968b
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-03-18 08:56:48 -0300

    feat: criation and implementation of the Footer component

--- src/app/components/header/page.jsx
+++ src/app/components/header/page.jsx
@@ -1,28 +1,28 @@
 import styles from "./header.module.css";
 
 export default function Header() {
-    return (
-        <header className={styles.header}>
-        <h1>Sala de Troféus do Corinthians</h1>
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-              <a href="#">Início</a>
-            </li>
-            <li>
-              <a href="#">História</a>
-            </li>
-            <li>
-              <a href="#">Campeonatos</a>
-            </li>
-            <li>
-              <a href="#">Ídolos</a>
-            </li>
-            <li>
-              <a href="#">Contato</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
+  return (
+    <header className={styles.header}>
+      <h1>Sala de Troféus do Corinthians</h1>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <a href="#">Início</a>
+          </li>
+          <li>
+            <a href="#">História</a>
+          </li>
+          <li>
+            <a href="#">Campeonatos</a>
+          </li>
+          <li>
+            <a href="#">Ídolos</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
